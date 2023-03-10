7ee4475c2ae023025308d1b7445756e9e6416696
LiN5BoY <2311549594@qq.com>
2023-03-10 23:33:47 +0800

    0310

--- Receive.cpp
+++ Receive.cpp
@@ -1,86 +1,85 @@
 #include<iostream>
-#include<Winsock2.h>
-#include<windows.h>
+#include<cstring>
+#ifdef _WIN32
 
-//UTF-8转GB2312
-char* U2G(const char* utf8){
-    int len = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
-    wchar_t* wstr = new wchar_t[len+1];
-    memset(wstr, 0, len+1);
-    MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wstr, len);
-    len = WideCharToMultiByte(CP_ACP, 0, wstr, -1, NULL, 0, NULL, NULL);
-    char* str = new char[len+1];
-    memset(str, 0, len+1);
-    WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, len, NULL, NULL);
-    if(wstr) delete[] wstr;
-    return str;
-}
-//GB2312转UTF-8
-char* G2U(const char* gb2312){
-    int len = MultiByteToWideChar(CP_ACP, 0, gb2312, -1, NULL, 0);
-    wchar_t* wstr = new wchar_t[len+1];
-    memset(wstr, 0, len+1);
-    MultiByteToWideChar(CP_ACP, 0, gb2312, -1, wstr, len);
-    len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
-    char* str = new char[len+1];
-    memset(str, 0, len+1);
-    WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
-    if(wstr) delete[] wstr;
-    return str;
-}
-//将目标远程主机的IP和端口信息填入sockaddr：
-sockaddr GetSockAddr(uint8_t b1, uint8_t b2, uint8_t b3, uint8_t b4, uint16_t inPort){
-    sockaddr addr;
-    sockaddr_in *addrin = reinterpret_cast<sockaddr_in*>(&addr);
-    addrin->sin_family = AF_INET;
-    addrin->sin_addr.S_un.S_un_b.s_b1 = b1;
-    addrin->sin_addr.S_un.S_un_b.s_b2 = b2;
-    addrin->sin_addr.S_un.S_un_b.s_b3 = b3;
-    addrin->sin_addr.S_un.S_un_b.s_b4 = b4;
-    addrin->sin_port = htons(inPort);
+#include<winsock2.h>
+#else
 
-    return addr;
-}
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+typedef int SOCKET;
+
+#endif
 
 int main(){
-    //先进行激活
-    ////WWSAStartup函数：第一个参数代表使用的版本号，该函数会向第二个参数填入被激活的socket库的信息
+#ifdef _WIN32
     WSADATA wsd;
+    //WWSAStartup函数：第一个参数代表使用的版本号，该函数会向第二个参数填入被激活的socket库的信息
     if(WSAStartup(MAKEWORD(2, 2), &wsd)){
         std::cout << "WSAStartup Error" << std::endl;
         exit(-1);
-    }
-    //创建socket，使用ipv4协议，报文为离散报文，默认协议（udp）
-    SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, 0);
+    }   
+#endif
+    //socket函数：(int af,int type,int protocol)
+    //af：使用协议族，如AF_INET为Ipv4,AF_INET6表示IPV6等
+    //type：指明通过socket发送和接受分组的形式。如SOCK_STREAM表示有序，可靠的数据流分段；SOCK_DGRAM表示离散的报文；SOCK_RAW表示数据头部可以由应用层自定义
+    //protocol：指明发送数据使用什么协议。IPPROTO_UDP；IPPOTO_TCP；IPPROTO_IP;0表示根据socket类型选择默认协议。
+    
+    //像这里，那么就是选择了ipv4协议，离散的报文，默认协议（udp）
+    SOCKET udpSocket = socket(AF_INET, SOCK_DGRAM, 0); 
+
+    //下面属于bind函数的范围，那么参数为：
+    //(SOCKET sock,const sockaddr *address,int address_len)
+    //sock:绑定socket
+    //address:是指发送数据包的源地址！而不是发送目的地地址
+    //address_len:存储address的sockaddr结构体大小。bind成功时返回0，出现错误时返回-1
+    //给端口号赋值0，并且告诉socket库找一个未被使用的端口并绑定
+    //如果一个进程试图使用一个未绑定的socket发送数据，网络库将自动为socket绑定一个可用的端口号
+    //对于服务器来说手动调用bind绑定是必须的，而对于客户端来说通常是没有必要的。
+    sockaddr_in sain;
+    sain.sin_family = AF_INET;
+    sain.sin_addr.s_addr = htonl(INADDR_ANY);
+    sain.sin_port = htons(atoi("50002"));
+    if(bind(udpSocket, (sockaddr *)&sain, sizeof(sockaddr)) == -1){
+        std::cout << "绑定失败" << std::endl;
+    }   
     const size_t BufMaxSize = 1000;
-    char buf[BufMaxSize] = {};
-        sockaddr toAddr = GetSockAddr(192, 168, 0, 182, 50002); 
-    int toAddrLen = sizeof(sockaddr);
-    std::cout << ">>> ";
+    char buf[BufMaxSize] = {}; 
+    sockaddr fromAddr;
+#ifndef _WIN32
+    unsigned
+#endif
+    //recfrom函数：(SOCKET s,char buf,int len,int flags,struct sockaddr from,int *fromlen)
+    //s：查询数据的socket
+    //buf：接收的数据包的缓冲区
+    //len：buf可以存储的最大字节数。到达的数据包的剩余字节将被丢弃。
+    //flags：同sendto flags
+    //fromlen：from所指向的sockaddr的大小
+    int fromAddrLen = sizeof(sockaddr);
+    std::cout << "等待接收..." << std::endl;
     while(true){
-        if(std::cin >> buf){
-            char *buf_UTF8 = G2U(buf);
-
-            //sendTo函数的参数如下：
-            //int sendto(SOCKET s,const char buf,int len,int flags,const struct sockaddr to,int tolen);
-            //s:数据包应该使用的socket，如果没有绑定，socket库将自动绑定一个可用的端口。
-            //buf:带发送数据的起始地址的指针。可以是任何能够转为char*的数据类型
-            //len:待发送数据的大小，尽量避免发送数据大于1300字节的数据包
-            //flags:对控制发送的标志进行按位或者运算的结果，该值通常取0即可
-            //to:目标接收者的sockaddr。注意to的地址族必须和用于创建socket的地址族一致
-            //tolen:to的sockaddr的大小。对于IPV4,传入sizeof(sockaddr_in)即可。
-            //sendto操作成功返回等待发送的数据长度（说明成功进入发送队列），否则返回-1。
-            sendto(udpSocket, buf_UTF8, strlen(buf_UTF8), 0, &toAddr, toAddrLen);
-            std::cout << "已发送!" <<std::endl;
-            std::cout << ">>> ";
+        if(recvfrom(udpSocket, buf, BufMaxSize, 0, &fromAddr, &fromAddrLen) != -1){
+            std::cout << "接收到数据：" << buf << std::endl;
             memset(buf, 0, sizeof(buf));
-        }
-    }
-
+        }   
+        else{
+            std::cout << "接收失败或发生错误！" << std::endl;
+            return -1; 
+        }   
+    }   
     //shutdown(udpSocket, SB_BOTH);
-    closesocket(udpSocket);
-
+    //关闭工作
+#ifdef _WIN32
     WSACleanup();
+    closesocket(udpSocket);
+#else
+    close(udpSocket);
+#endif
 
     return 0;
 }
